d538daf200e8deaaaa8460010b26a7c390770642
jwywoo <jwywoo26@gmail.com>
2023-02-13 21:46:57 -0500

    Second commit Blog: TDD review done

--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class BlogApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void myFirstTest() {
+		assertThat(1).isEqualTo(42);
 	}
 
 }
+
+
