1665a628e0b648d4d8c7a76037cbd3a19dd5f537
jwywoo <jwywoo26@gmail.com>
2023-02-22 20:50:37 -0500

    TDD: Serialization done

---
+++ src/main/java/com/example/blog/Post.java
@@ -0,0 +1,4 @@
+package com.example.blog;
+
+public record Post (Long id, String contents){
+}
--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,16 +1,39 @@
 package com.example.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+// @JsonTest tells spring to create Json Test class
+@JsonTest
 class BlogApplicationTests {
 
-	@Test
+	/*@Test
 	public void myFirstTest() {
 		assertThat(1).isEqualTo(42);
+	}*/
+
+	// @AutoWired tells spring to create an object of the code
+	// JacksonTester does serialization of deserialization of JSON objects
+	@Autowired
+	private JacksonTester<Post> json;
+
+	// Serialization test
+	@Test
+	public void postSerializationTest() throws IOException {
+		// Given Inputs
+		Post post = new Post(1L, "aaa");
+		// Whether the given inputs were serialized
+		assertThat(json.write(post)).isStrictlyEqualToJson("expected.json");
+		assertThat(json.write(post)).hasJsonPathNumberValue("@.id");
+		assertThat(json.write(post)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
+		
 	}
 
 }
