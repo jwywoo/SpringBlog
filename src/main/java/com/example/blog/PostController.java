5b1aeccfcd945dad25b8d692913588ed10da6c8e
jwywoo <jwywoo26@gmail.com>
2023-02-27 20:10:58 -0500

    Blog: RestController for Posts

---
+++ src/main/java/com/example/blog/PostController.java
@@ -0,0 +1,15 @@
+package com.example.blog;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
